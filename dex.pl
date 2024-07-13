3b37f4e1208c3dcab1f96bdefdaac576c654836e
nothing-c <halskdjf@gmail.com>
2024-07-13 18:45:02 -0500

    Fill out help function and get metadata going and set default file to dotfile

--- dex.pl
+++ dex.pl
@@ -1,16 +1,12 @@
 use v5.10; use Getopt::Std; use JSON::PP;
-our $F='./dex';
-#our %C=(
-#    flooble => {Address=>"12345 main st", Cell=>"123-456-7890"},
-#    barble => {Discord=>"\@xxx_barble_xxx", Email=>"bble84\@gmx.com"}
-#    );
+our $V="1.0";
+our $F='~/.dex';
+our %C; # contacts
 
 #name, platform, value
 sub add($$$) {
     my $n=shift; my $p=shift; my $v=shift; $C{$n}->{$p}=$v;
 }
-
-sub help() { say "Help thing here"; exit; }
 
 sub details($) { my $n=shift; die "Contact $n does not exist" unless exists $C{$n}; say "Name: $n"; while (my ($k, $v) = each %{$C{$n}}) { say "$k: $v";}}
 
@@ -20,12 +16,27 @@ sub existp($) { return True if exists $C{$_[0]}; return False; }
 
 sub listps($) { my $n=shift; die "Contact $n does not exist" unless exists $C{$n}; for (sort keys %{$C{$n}}) { say; } }
 
+sub help() {
+    say "dex v$V";
+    say "dex is a command-line rolodex";
+    say "Usage: dex [contact | -lh | -e contact | -o contact | -a contact -p platform -v value | -d contact [-p platform]]";
+    say "Options:";
+    say "  -a add or update a contact";
+    say "  -d delete a contact (or delete a platform from a contact with the -p switch)";
+    say "  -e check if contact exists";
+    say "  -h show this message";
+    say "  -l list all contacts";
+    say "  -o list platforms the contact is on";
+    say "  -p what platform the contact is on";
+    say "  -v the value for a given platform (email address, phone number, etc.)";
+    exit; }
+
 sub main() {
-    getopts 'lhe:u:p:v:a:d:o:';
+    getopts 'lhe:p:v:a:d:o:';
     listc if $opt_l;
     listps $opt_o if $opt_o; #need to impl
     say existp $opt_e if $opt_e;
-    if ($opt_a) { return "Need platform(s) and value(s) to add user!\n" unless ($opt_a && $opt_p && $opt_v); add $opt_a, $opt_p, $opt_v; } # I think this should work, but I need to test
+    if ($opt_a) { return "Need platform(s) and value(s) to add user!\n" unless ($opt_a && $opt_p && $opt_v); add $opt_a, $opt_p, $opt_v; }
     if ($opt_d) { return delete $C{$opt_d} unless $opt_p; delete $C{$opt_d}->{$opt_p}; }
     help if $opt_h;
     return '';
