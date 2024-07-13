da5e48e7aa4e9e9c1fcd08986026ae423d8143ff
nothing-c <halskdjf@gmail.com>
2024-07-13 16:32:25 -0500

    Detail addition created, detail dump created

---
+++ dex.pl
@@ -0,0 +1,18 @@
+use v5.10; use Getopt::Std;
+our %C;
+
+# $C{contact-name}={Address=>"12345 main st", Cell=>"123-456-7890"}; and so on
+
+#name, platform, value
+sub add($$$) {
+    my $n=shift; my $p=shift; my $v=shift; $C{$n}->{$p}=$v;
+}
+
+# load from file
+
+# dump details of person; existence is checked in details()
+if ($ARGV[0] !~ /-\w/) {say details $ARGV[0]; exit;}
+
+
+
+
