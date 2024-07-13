b38354ae58337088a4f99330526213ccc3812d8b
nothing-c <halskdjf@gmail.com>
2024-07-13 16:57:51 -0500

    Created dummy storage file; got loading JSON working

--- dex.pl
+++ dex.pl
@@ -1,4 +1,5 @@
-use v5.10; use Getopt::Std;
+use v5.10; use Getopt::Std; use JSON::PP;
+our $F='./dex';
 our %C=(
     flooble => {Address=>"12345 main st", Cell=>"123-456-7890"},
     barble => {Discord=>"\@xxx_barble_xxx", Email=>"bble84\@gmx.com"}
@@ -13,11 +14,12 @@ sub add($$$) {
 
 sub help() { say "Help thing here"; exit; }
 
-sub details($) { my $n=shift; die "Contact $n does not exist" unless exists $C{$n}; say $n; while (my ($k, $v) = each %{$C{$n}}) { say "$k: $v";}}
+sub details($) { my $n=shift; die "Contact $n does not exist" unless exists $C{$n}; say "Name: $n"; while (my ($k, $v) = each %{$C{$n}}) { say "$k: $v";}}
 
 help unless @ARGV;
 
 # load from file
+open my $f,"<",$F; %C=%{decode_json join '',(<$f>)};
 
 # dump details of person; existence is checked in details()
 if ($ARGV[0] !~ /-\w/) {say details $ARGV[0]; exit;}
