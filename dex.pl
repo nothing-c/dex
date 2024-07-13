761af3aea920a60a1db1d19723de5c690023e0f8
nothing-c <halskdjf@gmail.com>
2024-07-13 17:01:40 -0500

    Added contact listing

--- dex.pl
+++ dex.pl
@@ -1,11 +1,9 @@
 use v5.10; use Getopt::Std; use JSON::PP;
 our $F='./dex';
-our %C=(
-    flooble => {Address=>"12345 main st", Cell=>"123-456-7890"},
-    barble => {Discord=>"\@xxx_barble_xxx", Email=>"bble84\@gmx.com"}
-    );
-
-# $C{contact-name}={Address=>"12345 main st", Cell=>"123-456-7890"}; and so on
+#our %C=(
+#    flooble => {Address=>"12345 main st", Cell=>"123-456-7890"},
+#    barble => {Discord=>"\@xxx_barble_xxx", Email=>"bble84\@gmx.com"}
+#    );
 
 #name, platform, value
 sub add($$$) {
@@ -16,15 +14,18 @@ sub help() { say "Help thing here"; exit; }
 
 sub details($) { my $n=shift; die "Contact $n does not exist" unless exists $C{$n}; say "Name: $n"; while (my ($k, $v) = each %{$C{$n}}) { say "$k: $v";}}
 
+sub listc() { for (sort keys %C) { say $_; } }
+
 help unless @ARGV;
 
 # load from file
-open my $f,"<",$F; %C=%{decode_json join '',(<$f>)};
+open my $f,"<",$F; %C=%{decode_json join '',(<$f>)}; close $f;
 
 # dump details of person; existence is checked in details()
 if ($ARGV[0] !~ /-\w/) {say details $ARGV[0]; exit;}
 
-getopts 'h';
+getopts 'lh';
+listc if $opt_l;
 help if $opt_h;
 
 
