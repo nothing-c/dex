cfd4e75ece686e2dcd3dc9fb58381ca7fbad5c25
nothing-c <halskdjf@gmail.com>
2024-07-13 16:40:48 -0500

    detail dump mostly working

--- dex.pl
+++ dex.pl
@@ -1,5 +1,8 @@
 use v5.10; use Getopt::Std;
-our %C;
+our %C=(
+    flooble => {Address=>"12345 main st", Cell=>"123-456-7890"},
+    barble => {Discord=>"\@xxx_barble_xxx", Email=>"bble84\@gmx.com"}
+    );
 
 # $C{contact-name}={Address=>"12345 main st", Cell=>"123-456-7890"}; and so on
 
@@ -10,7 +13,7 @@ sub add($$$) {
 
 sub help() { say "Help thing here"; exit; }
 
-sub details($) { my $n=shift; say $n; while (my ($k, $v) = each %{$C{$n}}) { say "$k: $v";}}
+sub details($) { my $n=shift; die "Contact $n does not exist" unless exists $C{$n}; say $n; while (my ($k, $v) = each %{$C{$n}}) { say "$k: $v";}}
 
 help unless @ARGV;
 
